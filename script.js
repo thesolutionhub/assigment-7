d5f9769ade3b7e95a336b2abd8c54b03049c39e0
unknown <numan811a@gmail.com>
2022-01-23 01:49:53 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// 1 program ass 7
+// Write a js program to find maximum between two numbers. 
+// function maxInTwoNumber () {
+//     var num1 = prompt("enter first number")
+//     var num2 = prompt("enter first number")
+//     if (num1>num2) {
+//         console.log("number 1 is greater");
+//     }
+//     if (num1<num2) {
+//         console.log("number 2 is greater");
+//     }
+// }
+// sumOfTwoNumber();
+// Write a js program to find maximum between three numbers.
+// function MaxInThreeNumber () {
+//     var a = prompt("enter 1 number")
+//     var b = prompt("enter 2 number")
+//     var c = prompt("enter 3 number")
+//     if (a>b & a>c ) {
+//         console.log("A is big ");
+//     }
+//     if (b>c & b>a ) {
+//         console.log("B is big ");
+//     }
+//     if (c>a & c>b ) {
+//         console.log("C is big ");
+//     }
+// }
+// MaxInThreeNumber();
+// 3. Write a js program to check whether a number is negative, positive or zero.
+    // function numberCheck (){
+    //     var a = prompt("enter the number")    
+    //     if ( a == 0) {
+    //         console.log("number is equal to zero");
+    //     }
+    //     if ( a < 0) {
+    //         console.log("number is negative");
+    //     }
+    //     if ( a > 0) {
+    //         console.log("number is postive");
+    //     }
+    // }
+    // numberCheck ();
+    // 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+    // function numberDivide() {
+    //     var a = prompt("enter the number")
+    //     if (a%5 === 0) {
+    //         console.log("number is divide by 5");
+    //     }
+    //     if (a%11 === 0 ) {
+    //         console.log("number is divide by 11");
+    //     }
+    // }
+    // numberDivide();
+    // 5. Write a js program to check whether a number is even or odd.
+    // function numberEvenOdd() {
+    //     var a = prompt("enter the number")
+    //     if (a%2 === 0) {
+    //         console.log("the number is even");
+    //     }
+    //     else{
+    //         console.log("the number is odd");
+    //     }
+    // }
+    // numberEvenOdd()
+    // 6. Write a js program to check whether a year is leap year or not
+
+
+
+    
+
+
+
+
